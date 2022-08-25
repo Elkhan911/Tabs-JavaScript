9f283fe554cbedf0a170a053bfcfbf10a8a92e18
Elkhan911 <955pro@mail.ru>
2022-08-25 17:05:18 +0500

    realized js functions for toggle links

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ const link3 = document.querySelector("#_link3");
 const tabText1 = document.querySelector("#_tabText1");
 const tabText2 = document.querySelector("#_tabText2");
 const tabText3 = document.querySelector("#_tabText3");
+
+link1.addEventListener("click", function () {
+  tabText1.classList.add("content__text_active");
+  link1.classList.add("tabs__link_active");
+
+  tabText2.classList.remove("content__text_active");
+  tabText3.classList.remove("content__text_active");
+
+  link2.classList.remove("tabs__link_active");
+  link3.classList.remove("tabs__link_active");
+});
+
+link2.addEventListener("click", function () {
+  tabText2.classList.add("content__text_active");
+  link2.classList.add("tabs__link_active");
+
+  tabText1.classList.remove("content__text_active");
+  tabText3.classList.remove("content__text_active");
+
+  link1.classList.remove("tabs__link_active");
+  link3.classList.remove("tabs__link_active");
+});
+
+link3.addEventListener("click", function () {
+  tabText3.classList.add("content__text_active");
+  link3.classList.add("tabs__link_active");
+
+  tabText1.classList.remove("content__text_active");
+  tabText2.classList.remove("content__text_active");
+
+  link1.classList.remove("tabs__link_active");
+  link2.classList.remove("tabs__link_active");
+});
