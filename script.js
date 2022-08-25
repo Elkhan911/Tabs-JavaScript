62054355b69cd1a51ca2d4d98888887162e75057
Elkhan911 <955pro@mail.ru>
2022-08-25 16:41:16 +0500

    variables fo js

---
+++ script.js
@@ -0,0 +1,8 @@
+// ссылки как кнопка переключателей
+const link1 = document.querySelector("#_link1");
+const link2 = document.querySelector("#_link2");
+const link3 = document.querySelector("#_link3");
+
+const tabText1 = document.querySelector("#_tabText1");
+const tabText2 = document.querySelector("#_tabText2");
+const tabText3 = document.querySelector("#_tabText3");
